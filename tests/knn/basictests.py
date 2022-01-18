52d4d035d4acc35c0b31b077f9a026abc41a0ee7
BlueDespero <cezarytroska@gmail.com>
2022-01-18 18:32:23 +0100

    Applying the best found parameters to the test data.

--- tests/knn/basictests.py
+++ tests/knn/basictests.py
@@ -1,16 +1,17 @@
-from category_encoders import BinaryEncoder
+from category_encoders import BinaryEncoder, OneHotEncoder
 
 from Classification_algorithms.K_Nearest_Neighbors.k_nearest_neighbors import KNN
 from utils.data_preprocessing import get_data
 
-train_x, train_y, test, _ = get_data(["../../data/train.csv", "../../data/test.csv"], encoder=BinaryEncoder)
+train_x, train_y, test, _ = get_data(["../../data/train.csv", "../../data/test.csv"], encoder=OneHotEncoder)
 # train_x, train_y, test, _ = get_data(["../../data/train_num.csv", "../../data/test_num.csv"])
 
 knn = KNN()
-knn.train(train_x, train_y, k=10)
+knn.train(train_x, train_y, k=16, distance="matching")
 results = knn.classify(test)
 
-test['Survived'] = results
+test['Survived'] = results[0]
 
-test[['PassengerId', 'Survived']].to_csv('../../Classification_algorithms/Predictions/k_nearest_neighbors.csv',
-                                         index=False)
+test[['PassengerId', 'Survived']].to_csv(
+    '../../Classification_algorithms/K_Nearest_Neighbors/predictions/k_nearest_neighbors.csv',
+    index=False)
