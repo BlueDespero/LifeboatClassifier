1e04da826aa7b7fb1b411d34f14c22a78223a7f1
Cezary Troska <CTroska@noblesys.com>
2022-02-11 00:28:21 +0100

    Removing data loading

--- Summary/purityanalisys.py
+++ Summary/purityanalisys.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 from Classification_algorithms.Decision_Tree import purity_measures as pm
-
-all_data = pd.read_csv("../data/train.csv")
 
 
 def plot_results(col_names, purity_measures):
@@ -21,30 +18,30 @@ def plot_results(col_names, purity_measures):
     plt.show()
 
 
-def gini_summary():
+def gini_summary(data):
     col_names = []
     purity_measures = []
-    for name, data in all_data.iteritems():
+    for name, data in data.iteritems():
         col_names.append(name)
         purity_measures.append(pm.gini(data.value_counts()))
 
     plot_results(col_names, purity_measures)
 
 
-def entropy_summary():
+def entropy_summary(data):
     col_names = []
     purity_measures = []
-    for name, data in all_data.iteritems():
+    for name, data in data.iteritems():
         col_names.append(name)
         purity_measures.append(pm.entropy(data.value_counts()))
 
     plot_results(col_names, purity_measures)
 
 
-def mer_summary():
+def mer_summary(data):
     col_names = []
     purity_measures = []
-    for name, data in all_data.iteritems():
+    for name, data in data.iteritems():
         col_names.append(name)
         purity_measures.append(pm.mean_err_rate(data.value_counts()))
 
