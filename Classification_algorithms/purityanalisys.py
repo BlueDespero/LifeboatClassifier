8af7b07d6f99e1b19d704b7d1203e14c322173cb
Cezary Troska <CTroska@noblesys.com>
2022-02-11 01:08:07 +0100

    Changing the approach in purityanalisys.py

--- Classification_algorithms/purityanalisys.py
+++ Classification_algorithms/purityanalisys.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 import Classification_algorithms.Decision_Tree.purity_measures as pm
 
@@ -18,31 +19,16 @@ def plot_results(col_names, purity_measures):
     plt.show()
 
 
-def gini_summary(data):
+def purity_summary(data, purity_function):
     col_names = []
     purity_measures = []
     for name, data in data.iteritems():
         col_names.append(name)
-        purity_measures.append(pm.gini(data.value_counts()))
+        purity_measures.append(purity_function(data.value_counts()))
 
     plot_results(col_names, purity_measures)
 
 
-def entropy_summary(data):
-    col_names = []
-    purity_measures = []
-    for name, data in data.iteritems():
-        col_names.append(name)
-        purity_measures.append(pm.entropy(data.value_counts()))
-
-    plot_results(col_names, purity_measures)
-
-
-def mer_summary(data):
-    col_names = []
-    purity_measures = []
-    for name, data in data.iteritems():
-        col_names.append(name)
-        purity_measures.append(pm.mean_err_rate(data.value_counts()))
-
-    plot_results(col_names, purity_measures)
+if __name__ == "__main__":
+    full_data = pd.read_csv("../data/train.csv")
+    purity_summary(full_data, pm.gini)
