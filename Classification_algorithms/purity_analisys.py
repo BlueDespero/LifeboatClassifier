25536847b6c5e8d6f9af3598e20f166c264c6e6b
Cezary Troska <CTroska@noblesys.com>
2022-02-11 01:52:44 +0100

    Changing approach purity_analisys.py

--- Classification_algorithms/purity_analisys.py
+++ Classification_algorithms/purity_analisys.py
@@ -1,32 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import pandas as pd
+from itertools import cycle
 
 import Classification_algorithms.Decision_Tree.purity_measures as pm
 
+def purity_summary(data, purity_functions):
+    result_dataframe = pd.DataFrame()
 
-def plot_results(col_names, purity_measures):
-    purity_measures, col_names = zip(*sorted(zip(purity_measures, col_names)))
-    plt.rcdefaults()
-    fig, ax = plt.subplots()
+    for p_function, purity_name in purity_functions:
+        col_names = []
+        purity_measures = []
+        for name, col_data in data.iteritems():
+            col_names.append(name)
+            purity_measures.append(p_function(col_data.value_counts()))
 
-    # Example data
-    y_pos = np.arange(len(col_names))
-
-    ax.barh(y_pos, purity_measures, align='center', tick_label=col_names)
-    plt.show()
-
-
-def purity_summary(data, purity_function):
-    col_names = []
-    purity_measures = []
-    for name, data in data.iteritems():
-        col_names.append(name)
-        purity_measures.append(purity_function(data.value_counts()))
-
-    plot_results(col_names, purity_measures)
+        p_dataframe = pd.DataFrame(list(zip(cycle([purity_name]), purity_measures, col_names)),
+                                   columns=["purity_name", "purity_measure", "feature"])
+        result_dataframe = pd.concat([result_dataframe, p_dataframe])
 
 
 if __name__ == "__main__":
     full_data = pd.read_csv("../data/train.csv")
-    purity_summary(full_data, pm.gini)
+    purity_summary(full_data, [(pm.gini, "gini"), (pm.entropy, "entropy")])
